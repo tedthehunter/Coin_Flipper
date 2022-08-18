10f4e2b70ac094a5bf75c48f19694ed4016a67f7
RJ <weinheimer.raymond@gmail.com>
2022-08-18 15:53:04 -0700

    Made some progress on the tournament class and on the initialization functions, still need to clean up printing across all classes and run tests

--- success copy.py
+++ success copy.py
@@ -7,7 +7,7 @@ class player:
     self.coins = coins
     
   def __repr__(self):
-    return f'Player Object representing {self.name}'
+    return f'{self.name} Object'
     
   def status(self):
     if self.coins > 1:
@@ -26,6 +26,8 @@ class player:
       print(str(self.name) + ' has been disqualified!')
       return True
 
+
+## TRY TO SET A DEFAULT VALUE FOR PLAYER_B IN SUCH A WAY THAT A SINGLE PLAYER WILL AUTOMATICALLY GET A BYE
 class game:
   def __init__(self, player_a, player_b):
     self.player_a = player_a
@@ -35,7 +37,7 @@ class game:
     return f'Game Object between {self.player_a.name} and {self.player_b.name}'
 
   def play(self):
-    winner = random.choice(self.player_a, self.player_b)
+    winner = random.choice([self.player_a, self.player_b])
     if winner == self.player_a:
       print(str(self.player_a.name) + ' wins a coin from ' + str(self.player_b.name) + '!')
       self.player_a.coins += 1
@@ -49,92 +51,111 @@ class game:
       self.player_a.dq_check()
       print('\n')
 
-class round:
-  def __init__(self, player_list):
-    self.player_list = player_list
-    self.game_list = [game(random.choice(self.player_list.pop(), random.choice(self.player_list.pop()))) for player in self.player_list]
 
-  def pair_up(self):
-    for player in self.player_list:
-      if len(self.player_list) >= 2:
-        self.game_list.append(game(self.player_list.pop(0), self.player_list.pop(-1)))
-      else:
-        pass
+## ATTEMPTING TO INVALIDATE THIS BLOCK OF CODE ##
+# class round:
+#   def __init__(self, player_list):
+#     self.player_list = player_list
+#     self.game_list = [game(random.choice(self.player_list.pop(), random.choice(self.player_list.pop()))) for player in self.player_list]
 
-  def dq_check(self):
-    for game in self.game_list:
-      if game.player_a.disqualified == True:
-        self.player_list.append(game.player_b)
-      elif game.player_b.disqualified == True:
-        self.player_list.append(game.player_a)
-      else:
-        self.player_list.append(game.player_a)
-        self.player_list.append(game.player_b)
+#   def pair_up(self):
+#     for player in self.player_list:
+#       if len(self.player_list) >= 2:
+#         self.game_list.append(game(self.player_list.pop(0), self.player_list.pop(-1)))
+#       else:
+#         pass
+
+#   def dq_check(self):
+#     for game in self.game_list:
+#       if game.player_a.disqualified == True:
+#         self.player_list.append(game.player_b)
+#       elif game.player_b.disqualified == True:
+#         self.player_list.append(game.player_a)
+#       else:
+#         self.player_list.append(game.player_a)
+#         self.player_list.append(game.player_b)
    
-  def execute_round(self):
-    self.pair_up()
-    for game in self.game_list:
-      game.play()
-    self.dq_check()
-    return self.player_list
+#   def execute_round(self):
+#     self.pair_up()
+#     for game in self.game_list:
+#       game.play()
+#     self.dq_check()
+#     return self.player_list
 
-#Attempt to make a func that initializes a number of players
+# this function initializes a number of players and names them
 def roster(number_of_players):
   player_list = []
   for i in range(1, number_of_players + 1):
-    player_list.append(player('Player ' +str(i), 1, i))
+    player_list.append(player(f'Player {str(i)}', 1))
   return player_list
 
+# this function sets the number of coins of all players in a list to the same number
 def setup_coins(player_list, number_of_coins):
   for player in player_list:
     player.coins = number_of_coins
+    
 
-def status_message(player_list):
-  print('\nRoster:')
-  for player in player_list:
-    player.status()
-  print('\n')
-
-# def tournament(number_of_players, number_of_coins):
-#   print('Welcome to the coin flipping tournament!')
-#   print('\nRoster:')
-#   player_list = roster(number_of_players)
-#   setup_coins(player_list, number_of_coins)
-#   status_message(player_list)
-#   print('\n')
-#   round_position = 1
-#   round_start()
-#   print(round_position)
-
+## WORKING ON TOURNAMENT CLASS -- BROKEN
 class tournament:
   def __init__(self, number_of_players, number_of_coins):
     self.number_of_players = number_of_players
     self.number_of_coins = number_of_coins
-    self.round_position = 0
+    self.round_position = 1
     self.player_list = roster(number_of_players)
     setup_coins(self.player_list, number_of_coins)
+    
+  def __repr__(self):
+    return f'Tournament Object between {self.number_of_players} players with {self.number_of_coins} coins'
+  
+  def status_message(self):
+    print('\nRemaining Players:')
+    for player in self.player_list:
+      player.status()
+    print('\n')
   
   def start_tournament(self):
     print('Welcome to the coin flipping tournament!')
-    self.next_round()
+    print(f'This tournament includes {self.number_of_players} players with {self.number_of_coins} coins.\n')
 
-  def round_start(self):
+  def execute_round(self):
+    print(f'Round {str(self.round_position)} begin!')
+    self.status_message()
+    game(random.choice(self.player_list), random.choice(self.player_list)).play()
     self.round_position += 1
-    current_round = round(self.player_list)
-    print('Round ' + str(self.round_position) + ' begin!')
-    status_message(self.player_list)
-    self.player_list = current_round.execute_round()
+    
+  def execute_tournament(self):
+    self.start_tournament()
+    i = 0
+    while i < 10:
+      self.execute_round()
+      i += 1
+    # self.announce_winner()
+    
 
-  def next_round(self):
-    while len(self.player_list) > 1:
-      self.round_start()
-    print('The tournament is complete!')
-    print(str(self.player_list[0].name) + ' is the winner!')
 
 
-test =  player('RJ', 5)
+# PLAYER CLASS TESTS SUCCESSFUL ***
+# test =  player('RJ', 5)
+# print(test)
+# print(test.name)
+# print(test.coins)
+# test.status()
+# print(test.dq_check())
+
+## GAME CLASS TESTS SUCCESSFUL ***
+# rj = player('RJ', 2)
+# derrick = player('Derrick', 2)
+# test = game(rj, derrick)
+# print(test)
+# print(test.player_a)
+# print(test.player_b)
+# test.player_a.status()
+# test.player_b.status()
+# test.play()
+# test.player_a.status()
+# test.player_b.status()
+
+test = tournament(4, 2)
 print(test)
-print(test.name)
-print(test.coins)
-test.status()
-print(test.dq_check())
+print(test.player_list)
+test.execute_tournament()
